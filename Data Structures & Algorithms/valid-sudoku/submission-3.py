2d5f16b771da659d1dce3fd1870243465a7be32b
An Van <121452010+realpyop@users.noreply.github.com>
2026-08-30 17:37:58 -0400

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == ".":
+                    continue
+                if board[r][c] in rows[r]:
+                    return False
+                if board[r][c] in cols[c]:
+                    return False
+                if board[r][c] in squares[(r//3, c//3)]:
+                    return False
+                
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+        return True
+
+# Time: O(n^2)  -   Have to go through the matrix
+# Space: O(n^2) -   Have to store every value in the matrix
